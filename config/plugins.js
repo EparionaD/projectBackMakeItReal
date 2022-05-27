dcb4c622f3b70ab244f97262dd5d8bb1431b33a5
Ernesto Pariona Díaz <eparionad@gmail.com>
2022-05-26 21:34:37 -0500

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,29 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "eparionad@gmail.com",
+        defaultReplyTo: "eparionad@gmail.com",
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
